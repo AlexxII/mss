5a07ffa08a9e9720667ef8ac169dff6205696cf8
MainAdmin <MainAdmin@local.su>
2021-05-24 17:52:59 +0300

    update

---
+++ src/components/EventChain/EventChainEx.js
@@ -0,0 +1,74 @@
+import React, { useState, useCallback } from 'react';
+import ReactFlow, {
+  addEdge,
+  isNode,
+} from 'react-flow-renderer';
+import dagre from 'dagre';
+
+import initialElements from './elements';
+
+const nodeWidth = 172;
+const nodeHeight = 56;
+const RANFDIR = 'LR'
+
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+const getLayoutedElements = (elements) => {
+  dagreGraph.setGraph({ rankdir: RANFDIR });
+
+  elements.forEach((el) => {
+    if (isNode(el)) {
+      dagreGraph.setNode(el.id, { width: nodeWidth, height: nodeHeight });
+    } else {
+      dagreGraph.setEdge(el.source, el.target);
+    }
+  });
+
+  dagre.layout(dagreGraph);
+  console.log(dagreGraph);
+
+  return elements.map((el) => {
+    if (isNode(el)) {
+      const nodeWithPosition = dagreGraph.node(el.id);
+      el.targetPosition = 'left'
+      el.sourcePosition = 'right'
+      el.position = {
+        x: nodeWithPosition.x - nodeWidth / 2 + Math.random() / 1000,
+        y: nodeWithPosition.y - nodeHeight / 2,
+      };
+    }
+
+    return el;
+  });
+};
+
+const layoutedElements = getLayoutedElements(initialElements);
+
+const LayoutFlow = () => {
+  const [elements, setElements] = useState(layoutedElements);
+  const onConnect = (params) =>
+    setElements((els) =>
+      addEdge({ ...params, type: 'smoothstep', animated: true }, els)
+    );
+  const onLayout = useCallback(
+    (direction) => {
+      const layoutedElements = getLayoutedElements(elements, direction);
+      setElements(layoutedElements);
+    },
+    [elements]
+  );
+
+  return (
+    <div className="event-chain">
+      <ReactFlow
+        elements={elements}
+        onConnect={onConnect}
+        connectionLineType="smoothstep"
+      />
+    </div>
+  );
+};
+
+export default LayoutFlow;
+
