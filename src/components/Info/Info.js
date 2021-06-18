9d603559f58bba6fbf2e8fb081b57fe759afa7b4
MainAdmin <MainAdmin@local.su>
2021-06-18 11:48:36 +0300

    update

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,9 +1,11 @@
-import React, { memo } from 'react'
+import React, { Fragment, memo } from 'react'
 
 
 const Info = () => {
   return (
-    <h3>Информация</h3>
+    <Fragment>
+      <h3>Информация</h3>
+    </Fragment>
   )
 }
 
--- src/constants/inarray.js
+++ src/constants/inarray.js
@@ -2,16 +2,31 @@ const eventsTypes = [
   {
     id: '1',
     title: 'ОСС',
+    comments: 'Организация специоальной связи',
+    responsible: {
+      person: '',
+      telephone: ''
+    },
     order: 1
   },
   {
     id: '2',
     title: 'СИО',
+    comments: 'Специальное информационное обеспечение',
+    responsible: {
+      person: '',
+      telephone: ''
+    },
     order: 2
   },
   {
     id: '3',
     title: 'ИБ',
+    comments: 'Информационная безопасность',
+    responsible: {
+      person: '',
+      telephone: ''
+    },
     order: 3
   },
 ]
