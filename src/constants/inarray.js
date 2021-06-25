390ffe63afc87735039bb8e8276c9e738f2e8003
MainAdmin <MainAdmin@local.su>
2021-06-25 15:51:07 +0300

    update

--- src/constants/inarray.js
+++ src/constants/inarray.js
@@ -29,5 +29,55 @@ const eventsTypes = [
     },
     order: 3
   },
+  {
+    id: '4',
+    title: 'ТОб',
+    comments: 'Территориальная оборона',
+    responsible: {
+      person: 'Шаповалов Г.Н.',
+      telephone: 'АТС-ОГВ (0812) 30-37'
+    },
+    order: 3
+  },
+  {
+    id: '5',
+    title: 'КО',
+    comments: 'Кадровое обеспечение',
+    responsible: {
+      person: 'Вашкевич С.А.',
+      telephone: 'АТС-ОГВ (0812) 30-41'
+    },
+    order: 3
+  },
+  {
+    id: '6',
+    title: 'МО',
+    comments: 'Медицинское обеспечение',
+    responsible: {
+      person: 'Блудов С.В.',
+      telephone: 'АТС-ОГВ (0812) '
+    },
+    order: 3
+  },
+  {
+    id: '7',
+    title: 'МПО',
+    comments: 'Морально-психологическое обеспечение',
+    responsible: {
+      person: 'Литвинова Н.В.',
+      telephone: 'АТС-ОГВ (0812) '
+    },
+    order: 3
+  },
+  {
+    id: '8',
+    title: 'МТО',
+    comments: 'Материально-техническое обеспечение',
+    responsible: {
+      person: 'Ниденс Л.В.',
+      telephone: 'АТС-ОГВ (0812) 30-37'
+    },
+    order: 3
+  },
 ]
 export default eventsTypes
